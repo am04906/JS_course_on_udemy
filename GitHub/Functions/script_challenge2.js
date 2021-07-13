77b80e31e0d1a2515512754be9d80115a2f24974
Alexander Martynovich <alexander_martynovich@ibi.com>
2021-07-13 15:41:33 -0400

    use querySelector

--- GitHub/Functions/script_challenge2.js
+++ GitHub/Functions/script_challenge2.js
@@ -16,7 +16,7 @@
   const header = document.querySelector('h1');
   header.style.color = 'red';
 
-  document.getElementsByTagName('body')[0].addEventListener('click', function() {
+  document.querySelector('body').addEventListener('click', function() {
     header.style.color === 'blue' ? header.style.color = 'red' : header.style.color = 'blue';
   })
 })();
